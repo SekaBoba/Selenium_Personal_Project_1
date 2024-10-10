35a4e87b94d563c0ef7c1c1604375d1000bcee91
slobodanka.mihajlov <mihajlov.slobodanka@yahoo.com>
2024-10-10 11:48:59 +0200

    Selenium_Personal_Project_1

---
+++ page_objects/base_page.py
@@ -0,0 +1,30 @@
+
+from selenium.common import NoSuchElementException
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
+class BasePage:
+    #Locators
+    __url = "https://sampleapp.tricentis.com/101/index.php"
+    __header_get_your = "//div[@id='site-content']//ul[@class='slides']/li[2]//h2[@class='slide-title']"
+
+    def __init__(self, driver: WebDriver):
+        self._driver = driver
+
+    def open(self):
+        self._driver.get(self.__url)
+
+    def _find(self, locator: tuple) -> WebElement:
+        return self._driver.find_element(*locator)
+
+    def _is_displayed(self, locator: tuple) -> bool:
+        try:
+            return self._find(locator).is_displayed()
+        except NoSuchElementException:
+            return False
+
+    def find_get_your_header(self):
+        self._driver.find_element(self.__header_get_your)
+
+    def is_get_your_header_displayed(self) -> bool:
+        return super()._is_displayed(self.__header_get_your)
---
+++ tests/test_vehicle_insurance_application.py
@@ -0,0 +1,15 @@
+import pytest
+from page_objects.base_page import BasePage
+
+
+class TestProject1:
+
+    @pytest.mark.debug
+    def test1_confirm_vehicle_insurance_application_is_opened(self,driver):
+        base_page=BasePage(driver)
+        base_page.open()
+        assert base_page.is_get_your_header_displayed()
+
+
+
+
